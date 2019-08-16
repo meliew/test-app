de2477df4fe746a68e8e73c57c2457c309d4b151
Melissa Williams <melissa@brandmeetscopy.com>
2019-08-15 18:58:21 -0700

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,18 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
+
+  get "/" do
+    erb :index
+  end
+
+  get "/test" do
+    erb :test
+  end
+
+end
